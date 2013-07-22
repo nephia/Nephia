58394d61a33ea4ec337484965f9784a9ed087b03
ytnobody <ytnobody@gmail.com>
2013-07-22 11:59:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Voson - It's new $module
+
+# SYNOPSIS
+
+    use Voson;
+
+# DESCRIPTION
+
+Voson is ...
+
+# LICENSE
+
+Copyright (C) ytnobody.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+ytnobody <ytnobody@gmail.com>
---
+++ lib/Voson.pm
@@ -0,0 +1,39 @@
+package Voson;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Voson - A mini-sized WAF that aimed to substitute for Nephia
+
+=head1 SYNOPSIS
+
+    use Voson;
+
+=head1 DESCRIPTION
+
+Voson is ...
+
+=head1 LICENSE
+
+Copyright (C) ytnobody.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+ytnobody E<lt>ytnobody@gmail.comE<gt>
+
+=cut
+
