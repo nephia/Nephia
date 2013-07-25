342be49521b02895ce1e85a2e119eb68f27f6b22
ytnobody <ytnobody@gmail.com>
2013-07-25 16:48:50 +0900

    add HashHandler

--- cpanfile
+++ cpanfile
@@ -1,9 +1,11 @@
 requires 'perl', '5.008001';
 requires 'Scalar::Util';
 requires 'Module::Load';
+requires 'Router::Simple';
 requires 'Plack';
 
 on 'test' => sub {
     requires 'Test::More', '0.98';
+    requires 'JSON';
 };
 
---
+++ lib/Voson/Plugin/HashHandler.pm
@@ -0,0 +1,33 @@
+package Voson::Plugin::HashHandler;
+use strict;
+use warnings;
+use parent 'Voson::Plugin';
+use Data::Dumper;
+
+sub new {
+    my ($class, %opts) = @_;
+    my $self = $class->SUPER::new(%opts);
+    $self->app->{hash_handler} = $opts{handler} || $self->can('_default_handler');
+    my $app = $self->app;
+    $app->action_chain->after('Core', HashHandler => $self->can('handle_hash'));
+    return $self;
+}
+
+sub handle_hash {
+    my ($app, $context) = @_;
+    my $res = $context->get('res');
+    if (ref($res) eq 'HASH') {
+        $res = $app->{hash_handler}->($res);
+        $context->set(res => $res);
+    }
+    return $context;
+}
+
+sub _default_handler {
+    local $Data::Dumper::Terse = 1;
+    my $res = shift;
+    warn 'Plugin::HashHandler wants "handler" attr';
+    return [200, [], Dumper($res)];
+}
+
+1;
