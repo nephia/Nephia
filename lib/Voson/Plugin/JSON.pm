0674d041d621f8530048df08f5461408359c273a
ytnobody <ytnobody@gmail.com>
2013-08-12 16:53:51 +0900

    JSON API Security fix

--- lib/Voson/Plugin/JSON.pm
+++ lib/Voson/Plugin/JSON.pm
@@ -18,7 +18,14 @@ sub json_res {
     my ($self, $context) = @_;
     return sub ($) {
         $context->set(res => [
-            200, ['Content-Type' => 'applcation/json'], $self->app->{json_obj}->encode($_[0])
+            200, 
+            [
+                'Content-Type'           => 'applcation/json',
+                'X-Content-Type-Options' => 'nosniff',  ### For IE 9 or later. See http://web.nvd.nist.gov/view/vuln/detail?vulnId=CVE-2013-1297
+                'X-Frame-Options'        => 'DENY',     ### Suppress loading web-page into iframe. See http://blog.mozilla.org/security/2010/09/08/x-frame-options/
+                'Cache-Control'          => 'private',  ### no public cache
+            ], 
+            $self->app->{json_obj}->encode($_[0])
         ]);
     };
 }
